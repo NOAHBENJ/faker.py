5908914f66801e8b0ab46a7f29b8ed1edad5b24c
NOAHBENJ <82064301+NOAHBENJ@users.noreply.github.com>
2022-02-02 22:09:49 +1100

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,22 @@
+import random
+
+I = str(random.randint(0, 9))
+II = str(random.randint(0, 9))
+III = str(random.randint(0, 9))
+IV = str(random.randint(0, 9))
+V = str(random.randint(0, 9))
+VI = str(random.randint(0, 9))
+VII = str(random.randint(0, 9))
+VIII = str(random.randint(0, 9))
+IX = str(random.randint(0, 9))
+X = str(random.randint(0, 9))
+XI = str(random.randint(0, 9))
+XII = str(random.randint(0, 9))
+XIII = str(random.randint(0, 9))
+XIV = str(random.randint(0, 9))
+XV = str(random.randint(0, 9))
+XVI = str(random.randint(0, 9))
+
+number = I + II + III + IV + " " + V + VI + VII + VIII + " " + IX + X + XI + XII + " " + XIII + XIV + XV + XVI
+
+ccv = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
