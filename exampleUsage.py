3b40a596e81b316f6431013d990d51bba8e19005
NOAHBENJ <82064301+NOAHBENJ@users.noreply.github.com>
2022-02-03 16:37:35 +1100

    Update comments and usage
    Fixed up using faker.py

--- exampleUsage.py
+++ exampleUsage.py
@@ -1,13 +1,20 @@
 import faker
 import cards
 
-cc = cards.number
-
+# Getting the first name process to run, if you don't do that, then firstname will not work, it will be null or error.
 defFunc = faker.firstName()
 firstName = faker.randomFirstName
 
+# Getting the card, same as comment above
+defCardFunc = faker.Card()
+Card = faker.randomCard
+
+# Getting the last name, same as comment above
 defLastFunc = faker.lastName()
 lastName = faker.randomLastName
 
+# Printint all the variables, you can modify this how you want, this is just an example, you dont need this many arguments when printing, i just do to make it look nice
 print("Name: " + firstName + " " + lastName)
-print("Number: " + cc + "\nCCV: " + cards.ccv)
+
+# With this print, unlike the above lines, this one has all the nice printing done for you.
+print(Card)
